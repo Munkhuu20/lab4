6346df0bb3b8e7d9a9d358c456c63530ca7afc23
M. Munkhsaihan <67896164+Munkhuu20@users.noreply.github.com>
2021-04-10 00:54:41 +0800

    Add files via upload

---
+++ components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="header-container">
+        <div className="header-text">Eheltsgeey</div>
+      </div>
+    );
+  }
+}
+export default Header;
